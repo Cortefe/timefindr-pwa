e488d04be5f0bc3bd67afe30a9b0ca306b8ee32f
Cortefe <75344229+Cortefe@users.noreply.github.com>
2025-08-12 14:54:03 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>{e.waitUntil(caches.open('tf-v2').then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.json','./service-worker.js','./icons/icon-192.png','./icons/icon-512.png'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
