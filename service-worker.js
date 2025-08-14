ae7baea94d9d01a2ff1c9904de9a62c7be6af7c2
Cortefe <75344229+Cortefe@users.noreply.github.com>
2025-08-13 23:37:17 -0600

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,2 +0,0 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('tf-stable-v3').then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'])))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
